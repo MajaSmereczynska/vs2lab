16d2b3e5778903eef249994dcad9f5dd5b8119ff
TheCataliasTNT2k <44349750+TheCataliasTNT2k@users.noreply.github.com>
2025-10-27 13:27:15 +0100

    Added solution

--- lab2/rpc/runcl.py
+++ lab2/rpc/runcl.py
@@ -1,16 +1,20 @@
 import rpc
 import logging
+import time
 
 from context import lab_logging
 
 lab_logging.setup(stream_level=logging.INFO)
+logger = logging.getLogger('vs2lab.rpc.cl')
 
 cl = rpc.Client()
 cl.run()
 
 base_list = rpc.DBList({'foo'})
-result_list = cl.append('bar', base_list)
+cl.append('bar', base_list, lambda x: print("Result: {}".format(x.value)))
 
-print("Result: {}".format(result_list.value))
+for _ in range(15):
+    time.sleep(1)
+    logger.info("Client sleeping")
 
 cl.stop()
